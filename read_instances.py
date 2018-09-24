d4fcc8dd7b534f874c56a327f57f8d40e18e4a8d
diegomartinez33 <da.martinez33@uniandes.edu.co>
2018-09-24 15:36:53 -0500

    other commit

---
+++ read_instances.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+
+import pickle
+import numpy as np
+import os
+import os.path as osp
+
+TesisPath = '/hpcfs/home/da.martinez33/Tesis'
+
+not_balanced_path = osp.join(TesisPath, 'Data', 'DOE_databases')
+balanced_path = osp.join(TesisPath, 'Data', 'DOE_databases', 'balancedData')
+
+files = ['dataBaseDict_SC_amp_10_0.pkl', 'dataBaseDict_SC_amp_30_0.pkl']
+
+
+print('---------------- NOT BALANCED DATA -------------------')
+for file in files:
+    print('\n\nFile: ', file)
+    file_path = osp.join(not_balanced_path,file)
+    database = pickle.load(open(file_path,'rb'))
+
+    fold_1 = database['FirstSC']['Targets']
+    fold_2 = database['SecondSC']['Targets']
+    fold_3 = database['ThirdSC']['Targets']
+
+    for inst in range(0,7):
+        cont = 0
+        for i in fold_1:
+            if i == inst:
+                cont += 1
+        print('Fold 1 --- ', 'Class: ', inst, 'instances: ', cont)
+        cont = 0
+
+        for i in fold_2:
+            if i == inst:
+                cont += 1
+        print('Fold 2 --- ', 'Class: ', inst, 'instances: ', cont)
+        cont = 0
+
+        for i in fold_3:
+            if i == inst:
+                cont += 1
+        print('Fold 3 --- ', 'Class: ', inst, 'instances: ', cont)
+        cont = 0
+
+print('\n\n\n')
+print('---------------- BALANCED DATA -------------------')
+for file in files:
+    print('\n\nFile: ', file)
+    file_path = osp.join(balanced_path,file)
+    database = pickle.load(open(file_path,'rb'))
+
+    fold_1 = database['FirstSC']['Targets']
+    fold_2 = database['SecondSC']['Targets']
+    fold_3 = database['ThirdSC']['Targets']
+
+    for inst in range(0,7):
+        cont = 0
+        for i in fold_1:
+            if i == inst:
+                cont += 1
+        print('Fold 1 --- ', 'Class: ', inst, 'instances: ', cont)
+        cont = 0
+
+        for i in fold_2:
+            if i == inst:
+                cont += 1
+        print('Fold 2 --- ', 'Class: ', inst, 'instances: ', cont)
+        cont = 0
+
+        for i in fold_3:
+            if i == inst:
+                cont += 1
+        print('Fold 3 --- ', 'Class: ', inst, 'instances: ', cont)
+        cont = 0
+
+
+
